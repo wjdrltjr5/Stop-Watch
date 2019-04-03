8d5ae5fc19159d82fde44a1b0aaaa26aea7b267b
dit08 <dit@imac-3.local>
2019-04-03 15:33:47 +0900

    stop watch

--- Stop Watch 01/ViewController.swift
+++ Stop Watch 01/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    var count = 0
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        timeLabel.text = String(count)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBAction func startBtnPressed(_ sender: Any) {
+        if !myTimer.isValid {  //true 이면 작동 false 이면 작동불가능
+            myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+            
+        }
+        print(myTimer.isValid)
+    }
+    
+    @IBAction func pauseBtnPressed(_ sender: Any) {
+    myTimer.invalidate()
+    }
+    @IBAction func stopBtnPressed(_ sender: Any) {
+    myTimer.invalidate()
+    count = 0
+    timeLabel.text = String(count)
+    print(myTimer.isValid)
+    }//변수는 소문자부터
+    @objc func updateTime(){
+        count+=1
+        timeLabel.text = String(count)
+    }
 }
 
